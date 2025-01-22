1c28da3af6a4f2243640e04be680816852028075
GCdM <59828466+GCdM@users.noreply.github.com>
2025-01-22 00:49:04 +0000

    Create vim options config

--- README.md
+++ README.md
@@ -4,15 +4,18 @@ My personal NVIM config, based on [kickstart.nvim](https://github.com/nvim-lua/k
 
 ## Todo
 
-- [ ] Read through all tutorials:
-  - [ ] nvim's `:Tutor`
-  - [ ] kickstarter's `init.lua`
+- [ ] Read through kickstarter's `init.lua`
 - [ ] Setup `lazygit`
   - [ ] Might be worth setting up `snacks.nvim`
+- [ ] Add `neo-tree`
+  - [ ] Try to update startup to open `neo-tree` over `netrw`
 - [ ] Break-up `init.lua` into distinct plugins
-  - [ ] Enable more `mini.nvim` modules (`animate` _question: how does it compare to `snacks.nvim` animate?)
+  - [ ] Enable more `mini.nvim` modules (`animate` _question: how does it compare to `snacks.nvim` animate?_)
 - [ ] Create install script
   - [ ] Backup existing `~/.config/nvim`
   - [ ] Install external dependencies (`gcc`, `make`, `ripgrep`, etc...)
   - [ ] Add Nerdfont
 - [ ] Merge old `maidiyi` into this repo
+  - [ ] Add vim settings
+  - [ ] Add lazyvim keymaps
+  - [ ] Add plugins
---
+++ lua/config/vim.lua
@@ -0,0 +1,60 @@
+-- [[ Setting options ]]
+-- NOTE: See `:help vim.opt`
+-- For more options, you can see `:help option-list`
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+
+vim.opt.scrolloff = 8
+vim.opt.showmode = false
+vim.opt.wrap = true
+vim.opt.breakindent = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.swapfile = true
+vim.opt.updatetime = 250
+vim.opt.undofile = true
+vim.opt.undodir = os.getenv 'HOME' .. '/.vim/backup'
+vim.opt.backupdir = os.getenv 'HOME' .. '/.vim/undo'
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- TODO: Set up partial integration with OS clipboard
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+-- vim.schedule(function()
+--   vim.opt.clipboard = 'unnamedplus'
+-- end)
+
+--[[ 
+    **Unknown settings**
+
+    Not sure what these do, but keeping them for now from `kickstarter.nvim`
+--]]
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+-- Decrease mapped sequence wait time
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
