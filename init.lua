206e6ff5120316c45ca6403cf4fc08e5aa1e2ec4
GCdM <59828466+GCdM@users.noreply.github.com>
2025-01-23 13:07:59 +0000

    Move kickstarter plugins into plugins

--- init.lua
+++ init.lua
@@ -33,40 +33,12 @@ require 'config.autocommands'
 require 'config.lazy'
 
 require('lazy').setup({
-  -- spec = { import = 'plugins' },
-  -- install = { colorscheme = { 'kanagawa-wave' } },
-  -- checker = { enabled = true },
-
-  -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
-  -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
-
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  { import = 'plugins' },
-  --
-  -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
-  -- Or use telescope!
-  -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-  -- you can continue same window with `<space>sr` which resumes last telescope search
+  import = 'plugins',
+  install = { colorscheme = { 'tokyonight' } },
+  -- { colorscheme = { 'tokyonight-moon' },
 }, {
+  --[[ 
+
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
@@ -85,7 +57,9 @@ require('lazy').setup({
       task = '📌',
       lazy = '💤 ',
     },
-  },
+  }, 
+
+  --]]
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
