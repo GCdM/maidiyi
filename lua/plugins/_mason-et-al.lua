dd957f1f077b2e5f1ad5622ac7ac3701ed76845a
GCdM <59828466+GCdM@users.noreply.github.com>
2025-01-27 14:09:54 +0000

    Update mason
    - Add nvim-ts-autotag
    - Replace typescript-tools w/ ts_ls

--- lua/plugins/_mason-et-al.lua
+++ lua/plugins/_mason-et-al.lua
@@ -27,12 +27,13 @@ return {
       -- Allows extra capabilities provided by nvim-cmp
       'hrsh7th/cmp-nvim-lsp',
 
-      -- Replacement for `typescript-language-server`
-      {
-        'pmizio/typescript-tools.nvim',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        opts = {},
-      },
+      -- -- Replacement for `typescript-language-server`
+      -- {
+      --   'pmizio/typescript-tools.nvim',
+      --   dependencies = { 'nvim-lua/plenary.nvim' },
+      --   opts = {},
+      -- },
+      { 'windwp/nvim-ts-autotag', opts = {} },
     },
     config = function()
       --  This function gets run when an LSP attaches to a particular buffer.
@@ -160,17 +161,17 @@ return {
             },
           },
         },
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
+        -- clangd = {},
+        -- gopls = {},
+        -- pyright = {},
+        -- rust_analyzer = {},
+        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       }
 
       -- Ensure the servers and tools above are installed
