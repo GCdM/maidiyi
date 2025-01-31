bc35be456046ca4ebf5e216df828d86814bc4788
GCdM <59828466+GCdM@users.noreply.github.com>
2025-01-31 15:31:31 +0000

    Move lazy setup to config/lazy.lua (from init.lua)

--- init.lua
+++ init.lua
@@ -32,35 +32,3 @@ require('config.keymaps')
 require('config.autocommands')
 
 require('config.lazy')
-require('lazy').setup({
-  import = 'plugins',
-}, {
-  install = { colorscheme = { 'tokyonight-moon' } },
-
-  --[[ 
-
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  }, 
-
-  --]]
-})
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
