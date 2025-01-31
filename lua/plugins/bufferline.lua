e966e561f2707c128678358ec9b48532567bdbc2
GCdM <59828466+GCdM@users.noreply.github.com>
2025-01-31 15:32:51 +0000

    Add bufferline plugin

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,16 @@
+return {
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    options = {
+      show_buffer_close_icons = false,
+      offsets = {
+        {
+          filetype = 'neo-tree',
+          separator = true,
+        },
+      },
+    },
+  },
+}
