93f53ee388d642ba3390990dc311799224af51be
GCdM <59828466+GCdM@users.noreply.github.com>
2025-01-31 15:32:16 +0000

    Add floating-help plugin

---
+++ lua/plugins/floating-help.lua
@@ -0,0 +1,28 @@
+return {
+  'Tyler-Barham/floating-help.nvim',
+  lazy = true,
+  opts = {},
+  keys = {
+    {
+      '<leader>H',
+      function()
+        require('floating-help').toggle()
+      end,
+      desc = 'Toggle Floating[*H*]elp window',
+    },
+  },
+
+  config = function()
+    -- Only replace cmds, not search; only replace the first instance
+    local function cmd_abbrev(abbrev, expansion)
+      local cmd = 'cabbr ' .. abbrev .. ' <c-r>=(getcmdpos() == 1 && getcmdtype() == ":" ? "' .. expansion .. '" : "' .. abbrev .. '")<CR>'
+      vim.cmd(cmd)
+    end
+
+    -- Redirect `:h` to `:FloatingHelp`
+    cmd_abbrev('h', 'FloatingHelp')
+    cmd_abbrev('help', 'FloatingHelp')
+    cmd_abbrev('helpc', 'FloatingHelpClose')
+    cmd_abbrev('helpclose', 'FloatingHelpClose')
+  end,
+}
