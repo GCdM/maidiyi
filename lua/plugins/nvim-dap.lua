3a3f7fda9d81551aa95930a7f4560e89bfb6d8a5
GCdM <59828466+GCdM@users.noreply.github.com>
2025-04-14 13:39:04 +0100

    Remove go language debugger delve

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -86,7 +86,7 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
+        -- 'delve',
       },
     })
 
@@ -128,13 +128,13 @@ return {
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
-    -- Install golang specific config
-    require('dap-go').setup({
-      delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has('win32') == 0,
-      },
-    })
+    -- -- Install golang specific config
+    -- require('dap-go').setup({
+    --   delve = {
+    --     -- On Windows delve must be run attached or it crashes.
+    --     -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
+    --     detached = vim.fn.has('win32') == 0,
+    --   },
+    -- })
   end,
 }
