0a4c1a85936e44af8e92cac09ffd2fb12251bb71
GCdM <59828466+GCdM@users.noreply.github.com>
2025-05-10 10:15:19 +0100

    Change default colourscheme to kanagawa-paper

--- lua/plugins/_styling.lua
+++ lua/plugins/_styling.lua
@@ -1,29 +1,14 @@
 return {
   -- Colourschemes
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    -- init = function()
-    --   -- Load the colorscheme here.
-    --   -- Like many other themes, this one has different styles, and you could load
-    --   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    --   vim.cmd.colorscheme('tokyonight-moon')
-    --
-    --   -- You can configure highlights by doing something like:
-    --   vim.cmd.hi('Comment gui=none')
-    -- end,
-  },
+  { 'folke/tokyonight.nvim' },
+  { 'rebelot/kanagawa.nvim' },
   {
-    'rebelot/kanagawa.nvim',
+    'thesimonho/kanagawa-paper.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme('kanagawa')
+      vim.cmd.colorscheme('kanagawa-paper-ink')
     end,
   },
-
   -- Useful for getting pretty icons, but requires a Nerd Font.
   { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
 
