80c6d392741e121140da8c507773e7a9da17885d
GCdM <59828466+GCdM@users.noreply.github.com>
2025-05-21 23:34:54 +0100

    Duplicate soft-wrap for txt files

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -12,10 +12,23 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- Enable line wrap for markdown files,
+-- Enable line wrap for text files,
 -- w/o inserting EOL characters.
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'text',
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+    -- vim.opt_local.list = false
+    vim.opt_local.textwidth = 0
+    vim.opt_local.wrapmargin = 0
+    vim.opt_local.formatoptions:remove('t')
+    -- vim.opt_local.columns = 80
+  end,
+})
+-- TODO: Remove duplication by using regex pattern correctly
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'txt',
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.linebreak = true
