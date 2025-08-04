3373f8a3442558c32fde973a569ca1bb09a6ea2a
GCdM <59828466+GCdM@users.noreply.github.com>
2025-08-04 10:36:46 +0100

    Fix avante model and remove image pasting support

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -9,7 +9,7 @@ return {
     providers = {
       claude = {
         endpoint = 'https://api.anthropic.com',
-        model = 'claude-4-0-sonnet',
+        model = 'claude-sonnet-4-20250514',
         extra_request_body = {
           temperature = 0.75,
           max_tokens = 4096,
@@ -32,23 +32,23 @@ return {
     'ibhagwan/fzf-lua', -- for file_selector provider fzf
     'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
     -- 'zbirenbaum/copilot.lua', -- for providers='copilot'
-    {
-      -- support for image pasting
-      'HakonHarnes/img-clip.nvim',
-      event = 'VeryLazy',
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
+    -- {
+    --   -- support for image pasting
+    --   'HakonHarnes/img-clip.nvim',
+    --   event = 'VeryLazy',
+    --   opts = {
+    --     -- recommended settings
+    --     default = {
+    --       embed_image_as_base64 = false,
+    --       prompt_for_file_name = false,
+    --       drag_and_drop = {
+    --         insert_mode = true,
+    --       },
+    --       -- required for Windows users
+    --       use_absolute_path = true,
+    --     },
+    --   },
+    -- },
     {
       -- Make sure to set this up properly if you have lazy=true
       'MeanderingProgrammer/render-markdown.nvim',
